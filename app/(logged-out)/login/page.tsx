85c3e0fffe635a13a06b63ef0801c847707ec2e0
dani784601 <dani784601@gmail.com>
2025-10-02 10:59:25 +0900

    feat: Enhance login page with card layout

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardAction,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PersonStandingIcon } from "lucide-react";
+
 export default function LoginPage() {
-  return <div>login</div>;
+  return (
+    <>
+      <PersonStandingIcon size={50} className="text-pink-500" />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your Support Me account</CardDescription>
+        </CardHeader>
+        <CardContent>Login Form</CardContent>
+        <CardFooter className="justify-between">
+          <small>Don&apos;t have an account?</small>
+          <Button variant="outline" size="sm" asChild>
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
