2931294999dab2b410dec021527f3d776e076de7
dani784601 <dani784601@gmail.com>
2025-10-16 14:29:27 +0900

    feat: Enhance Dashboard with Employees Stat Cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
+import EmployeesStat from "./components/employees-stat";
+``;
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+
 export default function DashboardPage() {
-  return <div>dashboard page</div>;
+  return (
+    <>
+      <Tabs defaultValue="employees">
+        <TabsList className="mb-4">
+          <TabsTrigger value="employees">Employees stats</TabsTrigger>
+          <TabsTrigger value="teams">Teams stats</TabsTrigger>
+        </TabsList>
+        <TabsContent value="employees">
+          <EmployeesStat />
+        </TabsContent>
+        <TabsContent value="teams">teams stats</TabsContent>
+      </Tabs>
+    </>
+  );
 }
