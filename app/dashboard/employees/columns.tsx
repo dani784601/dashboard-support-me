5c7f8d2e6203703ff126babad73a99a299ffa621
dani784601 <dani784601@gmail.com>
2025-10-22 10:59:07 +0900

    feat: Styling employees table

--- app/dashboard/employees/columns.tsx
+++ app/dashboard/employees/columns.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
 
 export type Employee = {
   id: number;
@@ -15,6 +18,28 @@ export const columns: ColumnDef<Employee>[] = [
   {
     accessorKey: "avatar",
     header: "",
+    cell: ({ row }) => {
+      const avatar: string = row.getValue("avatar");
+      const firstName: string = row.getValue("firstName");
+      const lastName: string = row.getValue("lastName");
+
+      return (
+        <Avatar>
+          {!!avatar && (
+            <Image
+              width={40}
+              height={40}
+              src={avatar}
+              alt={`${firstName} ${lastName}`}
+            />
+          )}
+          <AvatarFallback>
+            {firstName.charAt(0).toUpperCase() +
+              lastName.charAt(0).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      );
+    },
   },
   {
     accessorKey: "firstName",
@@ -31,5 +56,9 @@ export const columns: ColumnDef<Employee>[] = [
   {
     accessorKey: "isTeamLeader",
     header: "",
+    cell: ({ row }) => {
+      const isTeamLeader: boolean = row.getValue("isTeamLeader");
+      return isTeamLeader ? <Badge variant="success">Team Leader</Badge> : null;
+    },
   },
 ];
